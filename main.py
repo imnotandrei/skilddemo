146923f94089d3099e9cd2a708ecc5445daf08ec
sschwartz <imnotandrei@gmail.com>
2025-01-23 13:55:48 -0800

    Added code

--- main.py
+++ main.py
@@ -4,28 +4,34 @@ import boto3
 
 #define shortnames and bring in environment variables
 #region and account keys defined in local environment
-ec2 = boto3.client('ec2')
+ec2r = boto3.resource('ec2')
+ec2c = boto3.client('ec2')
 asg_dict = {}
 # Get amount of instances per type
 
 with open("asg.config") as asgconfig:
     for line in asgconfig:
+
         (key, val) = line.split()
-        asg_dict[int(key)] = val
+        asg_dict[str(key)] = val
 
-for n,t in asg_dict():
-    print (n,t)
+for instancetype, instancenumber in asg_dict.items():
+    #This now becomes our main loop to check if we match what is required.
 
-scaletype = "t2.nano"
+    print(instancetype, instancenumber)
+    scale_info = ec2c.describe_instances(
+        Filters=[
+            {'Name': 'instance-type', 'Values': [instancetype]},
+        ]
+    )
+    num_needed = int(instancenumber) - (len(scale_info) - 1)
+    if num_needed > 0:
+        ec2r.create_instances(InstanceType=[instancetype],MinCount=num_needed,MaxCount=num_needed)
 
-scale_info = ec2.describe_instances(
-    Filters=[
-        {'Name': 'instance-type', 'Values': [scaletype]},
-    ]
-)
 
-print (scale_info)
-print (len(scale_info)-1)
+
+#   print (num_needed)
+#    print(scale_info)
+#    print(len(scale_info) - 1)
 # compare to instance numbers required
 #
-
